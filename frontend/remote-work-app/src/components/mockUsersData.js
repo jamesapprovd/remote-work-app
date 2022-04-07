ad489c64e4efafa810f27f7e9170e262c7f35a2c
sharlxl <sharleneee@gmail.com>
2022-04-07 11:53:08 +0800

    employee card, column, main and some layout done

--- frontend/remote-work-app/src/components/mockUsersData.js
+++ frontend/remote-work-app/src/components/mockUsersData.js
@@ -1,55 +1,53 @@
-import { uuid } from "uuidv4";
-
 const mockUsersData = [
   {
-    userId: uuid(),
+    userId: 1,
     username: "James",
     email: "james@remotr.com",
     password: "james123",
     admin: true,
-    img: "",
+    img: "https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673-960x960.png",
     position: "Manager",
-    status: "",
+    status: "Offline",
     interactionCount: 0,
     workJournal: [
       {
-        date: 6 / 4 / 2022,
-        time: 2045,
+        date: "6/4/2022",
+        time: "2045",
         title: "Logos Creation",
         content: "I created a few awesome logos for the company!",
         private: true,
         comments: [
           {
             username: "Sing Hian",
-            date: 6 / 4 / 2022,
-            time: 2046,
+            date: "6/4/2022",
+            time: "2046",
             comment: "Nicely done, boss!",
           },
           {
             username: "Sharlyn",
-            date: 6 / 4 / 2022,
-            time: 2047,
+            date: "6/4/2022",
+            time: "2047",
             comment: "Awesome, boss!",
           },
         ],
       },
       {
-        date: 7 / 4 / 2022,
-        time: 0930,
+        date: "7/4/2022",
+        time: "930",
         title: "Puntuality",
         content: "Reached campus on time and got my coffee!",
         private: false,
         comments: [
           {
             username: "Sing Hian",
-            date: 7 / 4 / 2022,
-            time: 0935,
+            date: "7/4/2022",
+            time: "935",
             comment: "Great work, boss!",
           },
           {
             username: "Sharlyn",
-            date: 7 / 4 / 2022,
-            time: 0938,
+            date: "7/4/2022",
+            time: "938",
             comment: "Yay, boss!",
           },
         ],
@@ -57,15 +55,15 @@ const mockUsersData = [
     ],
     whiteFlag: [
       {
-        date: 6 / 4 / 2022,
-        time: 1901,
+        date: "6/4/2022",
+        time: "1901",
         title: "Need help with day 7 homework!",
         content: "Why doesn't my create work in postman?",
         comments: [
           {
             username: "Sing Hian",
-            date: 6 / 4 / 2022,
-            time: 1933,
+            date: "6/4/2022",
+            time: "1933",
             comment: "You didn't connect properly",
           },
         ],
@@ -73,54 +71,54 @@ const mockUsersData = [
     ],
   },
   {
-    userId: uuid(),
+    userId: 2,
     username: "Sing Hian",
     email: "singhian@remotr.com",
     password: "singhian123",
     admin: false,
-    img: "",
+    img: "https://www.nomadfoods.com/wp-content/uploads/2018/08/placeholder-1-e1533569576673-960x960.png",
     position: "Junior Executive",
-    status: "",
+    status: "Away",
     interactionCount: 0,
     workJournal: [
       {
-        date: 6 / 4 / 2022,
-        time: 2056,
+        date: "6/4/2022",
+        time: "2056",
         title: "React",
         content: "I created routes and pages!",
         private: true,
         comments: [
           {
             username: "James",
-            date: 6 / 4 / 2022,
-            time: 2100,
+            date: "6/4/2022",
+            time: "2100",
             comment: "Yay!",
           },
           {
             username: "Sharlyn",
-            date: 6 / 4 / 2022,
-            time: 2102,
+            date: "6/4/2022",
+            time: "2102",
             comment: "Yay!",
           },
         ],
       },
       {
-        date: 7 / 4 / 2022,
-        time: 1000,
+        date: "7/4/2022",
+        time: "1000",
         title: "Lunch",
         content: "What are we eating for lunch today?",
         private: false,
         comments: [
           {
             username: "James",
-            date: 7 / 4 / 2022,
+            date: "7/4/2022",
             time: 1002,
             comment: "It's still early, do your work!",
           },
           {
             username: "Sharlyn",
-            date: 7 / 4 / 2022,
-            time: 1005,
+            date: "7/4/2022",
+            time: "1005",
             comment: "Do your work!",
           },
         ],
@@ -128,15 +126,15 @@ const mockUsersData = [
     ],
     whiteFlag: [
       {
-        date: 6 / 4 / 2022,
-        time: 1901,
+        date: "6/4/2022",
+        time: "1901",
         title: "Help!",
         content: "Redux is so confusing!",
         comments: [
           {
             username: "Sharlyn",
-            date: 6 / 4 / 2022,
-            time: 1939,
+            date: "6/4/2022",
+            time: "1939",
             comment: "Watch some youtube videos",
           },
         ],
@@ -145,7 +143,5 @@ const mockUsersData = [
   },
   // {...}, {...} ...
 ];
-
 // whiteFlagsData = [];
-
 export default mockUsersData;
