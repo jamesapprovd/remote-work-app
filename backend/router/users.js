9e3a2efd377077877924dc57d80804ac9795205a
aviatorer <itsjamesteo@gmail.com>
2022-04-08 14:58:07 +0800

    Backend Router Set up -  User creation, login, logout, deletion OK
    router/users.js - User creation, login, logout, deletion OK
    middleware/auth.js - authentication for user login
    sessions set up and working

---
+++ backend/router/users.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const Users = require("../models/Users");
+const router = express.Router();
+const auth = require("../middleware/auth");
+
+const usernameOrPasswordError = {
+  status: "error",
+  message: "username or password error",
+};
+
+router.post("/create", async (req, res) => {
+  try {
+    req.body.password = await bcrypt.hash(req.body.password, 12);
+    const createdUser = await Users.create(req.body);
+    console.log("created user is: ", createdUser);
+    res.json({ status: "ok", message: "user created" });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json(usernameOrPasswordError);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findOne({ username: req.body.username });
+
+  if (user === null) {
+    console.log("user null");
+    return res.status(401).send(usernameOrPasswordError);
+  }
+
+  const result = await bcrypt.compare(password, user.password);
+  if (result) {
+    req.session.currentUser = user.username;
+    req.session.userId = user.id;
+    res.send({ status: "ok", message: "user logged in" });
+  } else {
+    req.session.currentUser = null;
+    req.session.userId = null;
+    res.status(401).send(usernameOrPasswordError);
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.json({ status: "ok", message: "logged out" });
+  });
+});
+
+//authentication
+router.get("/profile", auth, (req, res) => {
+  if (req.session.currentUser) {
+    res.json({ status: "ok", message: "profile" });
+  } else {
+    res.status(403).json({ status: "error", message: "please login" });
+  }
+});
+
+router.delete("/remove", async (req, res) => {
+  const { username } = req.body;
+  const message = await Users.deleteOne({ username }); //deleting based on username
+
+  if (message.deletedCount === 1) {
+    res.json({ status: "ok", message: "user deleted" });
+  } else {
+    res.json({ status: "error", message: "problems with deleting user" });
+  }
+});
+
+module.exports = router;
