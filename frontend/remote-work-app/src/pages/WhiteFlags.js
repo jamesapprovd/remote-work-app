9534dbf385c0ebefcd240bd36022482d0460f9c1
aviatorer <itsjamesteo@gmail.com>
2022-04-12 15:17:22 +0800

    Adding states and axios to White Flag page

--- frontend/remote-work-app/src/pages/WhiteFlags.js
+++ frontend/remote-work-app/src/pages/WhiteFlags.js
@@ -3,9 +3,63 @@ import NavBar from "../components/NavBar";
 import EmployeeColumn from "../components/EmployeeColumn";
 import InputBox from "../components/InputBox";
 import WhiteFlagCard from "../components/WhiteFlagCard";
+import { selectUser } from "../redux/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+// import { ADD_FLAG } from "../redux/userSlice";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 const WhiteFlags = () => {
   const [index, setIndex] = useState(null);
+  const [flag, setFlag] = useState({
+    whiteFlagID: "",
+    date: "",
+    time: "",
+    title: "",
+    content: "",
+    isSolved: false,
+    comments: "",
+  });
+
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  let name = user.username;
+  let position = user.position;
+  let interactionCount = user.interactionCount;
+
+  const onSubmitFlag = (event) => {
+    event.preventDefault();
+    const newFlag = {
+      whiteFlagID: uuidv4(),
+      date: new Date().toLocaleDateString(),
+      time: new Date().toLocalTimeStamp(),
+      title: flag.title,
+      content: flag.content,
+      isSolved: false,
+      comments: [],
+    };
+
+    let userId = user.userId;
+    axios
+      .post(
+        `http://127.0.0.1:5001/whiteFlags/new`,
+        {
+          newFlag,
+          userId,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        console.log(res.data);
+        console.log(res.data.status);
+        if (res.data.status === "ok") {
+          console.log("whiteflag hi5", res.data);
+        }
+      });
+    dispatch(ADD_FLAG(newFlag));
+  };
+
   return (
     <>
       <div className="flex flex-row">
@@ -18,7 +72,13 @@ const WhiteFlags = () => {
           <NavBar />
           <div className="flex flex-row">
             <div className="bg-red-500 basis-1/2 m-1">
-              <InputBox text="Get help: Post a White Flag" />
+              <InputBox
+                text="Get help: Post a White Flag"
+                title={flag.title}
+                content={flag.content}
+                setFlag={setFlag}
+                onSubmit={onSubmitFlag}
+              />
             </div>
             <div className="bg-green-500 basis-1/2 m-1 h-screen">
               White Flags
