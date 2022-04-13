0b1f07f7c31b0328f94518f29a48fd85ea0ce583
sharlxl <sharleneee@gmail.com>
2022-04-13 11:31:17 +0800

    my journal viewcard done

--- frontend/remote-work-app/src/components/CommentInputBox.js
+++ frontend/remote-work-app/src/components/CommentInputBox.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 const buttonStyle =
-  "text-sm border-2 border-purple rounded-md hover:bg-green hover:text-black mt-2 ml-2 px-1";
+  "self-center w-[50%] text-sm bg-green border-2 border-green rounded-md hover:border-purple hover:text-black mt-2 ml-2 px-1";
 
 const CommentInputBox = (props) => {
   return (
-    <form onSubmit={props.onSubmitComment}>
-      <textarea
-        className="border border-black m-2 text-center"
-        rows="5"
-        placeholder="comment here"
-        value={props.comment}
-        onChange={props.onChangeComment}
-      />
-      <button className={buttonStyle}>Post</button>
+    <form
+      className="border-y border-purple p-2"
+      onSubmit={props.onSubmitComment}
+    >
+      <label className="px-3 text-purple">Enter a new comment:</label>
+      <div className="flex flex-col">
+        <textarea
+          className="rounded-md border border-grey m-2 text-center focus:bg-lightgreen focus:outline-green"
+          rows="5"
+          placeholder="comment here"
+          value={props.comment}
+          onChange={props.onChangeComment}
+        />
+        <button className={buttonStyle}>Post</button>
+      </div>
     </form>
   );
 };
